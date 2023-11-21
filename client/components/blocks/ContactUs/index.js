28f025595f0da0405b344d7024f6f6ddb96da5fe
Ethienne Roy <ethienner@gmail.com>
2023-11-21 11:05:15 -0500

    initial commit

---
+++ client/components/blocks/ContactUs/index.js
@@ -0,0 +1,14 @@
+import delve from 'dlv';
+import Link from 'next/link';
+
+const ContactUs = ({ title, text, buttons, theme }) => {
+ return (
+  <div className={`bg-${theme}`}>
+   <h1>contact usss</h1>
+  </div>
+  );
+};
+
+ContactUs.defaultProps = {};
+
+export default ContactUs;
