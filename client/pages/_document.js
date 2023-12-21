b1237fb8ea546cb737e2acdf95634a2ff8d2e497
Ethienne Roy <ethienner@gmail.com>
2023-12-20 19:53:40 -0500

    added gtag

--- client/pages/_document.js
+++ client/pages/_document.js
@@ -1,5 +1,5 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document';
-
+import Script from 'next/script'
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx);
@@ -11,12 +11,22 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="shortcut icon" href="/favicon.png" />
+          <Script src="https://www.googletagmanager.com/gtag/js?id=G-Z5Y2JFF0HX" />
+          <Script id="google-analytics">
+            {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'G-Z5Y2JFF0HX');
+        `}
+          </Script>
         </Head>
         <body>
 
           <Main />
           <NextScript />
-          
+
         </body>
       </Html>
     );
